d5b16d42e509350ebb93159c0749c2a38d47f5e0
Prathamrjk <prathameshkulkarni710@gmail.com>
2023-01-31 11:52:51 +0530

    Header: Logo , Links Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <Container>
+    <Logo src="/images/logo.svg"></Logo>
+    <Menu>
+      <p><a href="#">Model S</a></p>
+      <p><a href="#">Model 3</a></p>
+      <p><a href="#">Model X</a></p>
+      <p><a href="#">Model Y</a></p>
+    </Menu>
+   </Container>
   )
 }
 
 export default Header
+
+
+const Container=styled.div`
+min-height:60px;
+top:0;
+left:0;
+right:0;
+display:flex;
+align-items:center;
+position:fixed;
+padding:0 20px;
+`
+const Logo=styled.img`
+
+`
+
+const Menu=styled.div`
+display:flex;
+align-item:center;
+justify-content:center;
+flex:1;
+p{
+  font-weight:600;
+  text-transform:uppercase;
+  padding: 0 10px;
+  flex-wrap:nowrap;
+}
+
+`
