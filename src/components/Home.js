c880e18db64f96ea44df1f9ac82e5142a97697cb
Prathamrjk <prathameshkulkarni710@gmail.com>
2023-01-31 09:34:23 +0530

    First Section Updated

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+import Section from './Section';
+export const Home = () => {
+  return (
+   <Container>
+    <Section></Section>
+    
+   </Container>
+  )
+}
+
+const Container=styled.div`
+max-height:100vh;
+max-width:100vw;
+
+`
